4479afb3d419b68c42e1266c53d31a8584b22408
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-04-28 15:03:52 +0900

    Split file commit

---
+++ dotinstall/sidebar.php
@@ -0,0 +1,11 @@
+<div id="sidebar">
+        <div class="widget">
+          <h3>カテゴリー</h3>
+          <ul>
+            <li><a href=""></a>item</li>
+            <li><a href="">item</a></li>
+            <li><a href=""></a>item</li>
+          </ul>
+        </div>
+      </div><!-- /sidebar -->
+    </div><!-- /main -->
