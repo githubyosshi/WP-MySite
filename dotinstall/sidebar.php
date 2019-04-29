7e10cb9c2c31942cf74c49740750ba9cf042710e
githubyosshi <Learn Git and GitHub without any code!kanrisya.shisutemu@gmail.com>
2019-04-29 10:57:58 +0900

    Supports widgets commit

--- dotinstall/sidebar.php
+++ dotinstall/sidebar.php
@@ -1,10 +1,3 @@
 <div id="sidebar">
-  <div class="widget">
-    <h3>カテゴリー</h3>
-    <ul>
-      <li><a href="">item1</a></li>
-      <li><a href="">item</a></li>
-      <li><a href="">item</a></li>
-    </ul>
-  </div>
+  <?php dynamic_sidebar(); ?>
 </div><!-- /sidebar -->
